66940f06ea0ad63667004dff30dd1d2694bdcd25
Sealman <castle2668@gmail.com>
2020-02-12 17:00:34 +0800

    remove console.log

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import VueAxios from 'vue-axios';
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css'
 import 'bootstrap';
-import VeeValidate from 'vee-validate'; // VeeValidate
-import zhTW from 'vee-validate/dist/locale/zh_TW'; // 中文
-import VueI18n from 'vue-i18n'; // vue-i18n
+import VeeValidate from 'vee-validate';
+import zhTW from 'vee-validate/dist/locale/zh_TW';
+import VueI18n from 'vue-i18n';
 
 import App from './App';
 import router from './router';
@@ -14,19 +14,17 @@ import './bus';
 import currencyFilter from './filters/currency';
 import dateFilter from './filters/date';
 
-// Swiper (全域)
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 import 'swiper/dist/css/swiper.css';
 Vue.use(VueAwesomeSwiper);
 
 Vue.use(VueAxios, axios);
-// vue-i18n
 Vue.use(VueI18n);
 const i18n = new VueI18n({
   locale: 'zhTW'
 })
 Vue.use(VeeValidate, {
-  events: 'input|blur', // 這是為了讓使用者離開該欄位時觸發驗證
+  events: 'input|blur',
   i18n,
   dictionary: {
     zhTW,
@@ -35,14 +33,14 @@ Vue.use(VeeValidate, {
 
 Vue.component('Loading', Loading);
 Vue.filter('currency', currencyFilter);
-Vue.filter('date', dateFilter); // 全域啟用
+Vue.filter('date', dateFilter);
 
 Vue.config.productionTip = false;
 
 axios.defaults.withCredentials = true;
 
 new Vue({
-  i18n, // 把 i18n 載入
+  i18n,
   el: '#app',
   components: {
     App
@@ -52,13 +50,9 @@ new Vue({
 })
 
 router.beforeEach((to, from, next) => {
-  // console.log('to', to);
-  // console.log('from', from);
-  // console.log('next', next);
   if (to.meta.requiresAuth) {
     const api = `${process.env.APIPATH}/api/user/check`;
     axios.post(api).then(response => {
-      // console.log(response.data);
       if (response.data.success) {
         next();
       } else {
